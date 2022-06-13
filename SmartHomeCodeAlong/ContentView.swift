752f501cc0b8559571052eac33a8f8eba2cfc96d
40314471 <cclarke60@qub.ac.uk>
2022-06-13 12:23:29 +0100

    Initial Commit

---
+++ SmartHomeCodeAlong/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SmartHomeCodeAlong
+//
+//  Created by Charles Richard Clarke on 13/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SmartHomeCodeAlong/SmartHomeCodeAlongApp.swift
@@ -0,0 +1,17 @@
+//
+//  SmartHomeCodeAlongApp.swift
+//  SmartHomeCodeAlong
+//
+//  Created by Charles Richard Clarke on 13/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct SmartHomeCodeAlongApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
